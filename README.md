fc90781405c541665dcad2f19ef447a1aed53f40
Elham Salamat <35340273+elham-salamat@users.noreply.github.com>
2021-12-07 15:33:38 +0100

    JS Variables updated
    A list of different pokemons including their name, types and height are added.

---
+++ README.md
@@ -0,0 +1,2 @@
+## Pokemon App
+-this app is a simple app providing information about some pokemons to the users. 
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
-alert('Hello World!');
+let pokemonList = [];
 
-let myFavoriteFood = "Pasta";
+pokemonList[0] = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: [
+        'grass', 'poison'
+    ]
+}
 
-document.write(myFavoriteFood); 
+pokemonList[1] = {
+    name: 'Charizard',
+    height: 1.7,
+    type: [
+        'fire', 'flying'
+    ]
+}
+
+pokemonList[2] = {
+    name: 'Kakuna',
+    height: 0.6,
+    type: [
+        'bug', 'poison'
+    ]
+}
+
+pokemonList[3] = {
+    name: 'Pidgey',
+    height: 0.3,
+    type: [
+        'flying', 'normal'
+    ]
+}
+
+
+pokemonList[4] = {
+    name: 'Pidgeot',
+    height: 1.5,
+    type: [
+        'flying', 'normal'
+    ]
+}
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + '\n');
+}
