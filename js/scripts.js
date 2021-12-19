0c5dc8f4d0db19964a474d44d18c6e94755d99fe
Elham Salamat <35340273+elham-salamat@users.noreply.github.com>
2021-12-19 15:52:37 +0100

    make the app interactive by adding the eventlistener to pokemons' list

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,86 @@
 
+//pokemonRepository creation and retrievement function
 let pokemonRepository = (function () {
-    let pokemonList = [];
 
+    // pokemonRepository creation
+    let pokemonList = [
+        {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: [
+              'grass', 'poison'
+        ]
+        },
+        {
+        name: 'Charizard',
+        height: 1.7,
+        type: [
+              'fire', 'flying'
+        ]
+        },
+        {
+        name: 'Kakuna',
+        height: 0.6,
+        type: [
+              'bug', 'poison'
+        ]
+        },
+        {
+        name: 'Pidgey',
+        height: 0.3,
+        type: [
+              'flying', 'normal'
+        ]
+        },
+        {
+        name: 'Pidgeot',
+        height: 1.5,
+        type: [
+              'flying', 'normal'
+        ]
+        }
+    ]
+
+    //function to add new object (pokemon) to pokemonRepository
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
+    //function to retrieve the pokemons' list available in pokemonRepository
     function getAll() {
         return pokemonList
     }
 
+    //function to create and represent the pokemon item in web browser
+    function addListItem(pokemon) {
+        let pokemonContainer = document.querySelector('.pokemon-list');
+        let ul = pokemonContainer;
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon');
+        listItem.appendChild(button);
+        //onclick functionality of listItem (pokemon name)
+        onClick(button, pokemon);
+        ul.appendChild(listItem);
+    }
+
+    //defining onclick function to represent the pokemon details on click event
+    function onClick(button, pokemon) {
+        button.addEventListener('click', function(event) {
+            console.log(`${pokemon.name} ${pokemon.height}`);
+        })
+    }
+
     return {
-      add: add,
-      getAll: getAll
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
     }
 
 })();
 
-pokemonRepository.add({
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: [
-        'grass', 'poison'
-    ]
-});
-
-pokemonRepository.add({
-    name: 'Charizard',
-    height: 1.7,
-    type: [
-        'fire', 'flying'
-    ]
-});
-
-pokemonRepository.add({
-    name: 'Kakuna',
-    height: 0.6,
-    type: [
-        'bug', 'poison'
-    ]
-});
-
-pokemonRepository.add({
-    name: 'Pidgey',
-    height: 0.3,
-    type: [
-        'flying', 'normal'
-    ]
-});
-
-pokemonRepository.add({
-    name: 'Pidgeot',
-    height: 1.5,
-    type: [
-        'flying', 'normal'
-    ]
-});
-
-// extracting the tallest pokemon in the pokemonRepository
-let pokemonHeights = [];
-
-pokemonRepository.getAll().forEach(heightArrayCreation);
-
-function heightArrayCreation(pokemon) {
-    pokemonHeights.push(pokemon.height);
-}
-
-let maxHeight = Math.max(...pokemonHeights);
-
-// appending the list items(pokemons' list) to ul element
-function function1() {
-    let ul = document.getElementById("pokemonlist");
-
-    // creating the pokemon list available in repository
-    pokemonRepository.getAll().forEach(pokemonInfosRepresentation);
-
-    function pokemonInfosRepresentation(pokemon) {
-        let li = document.createElement("li");
-
-        // highlighting the tallest pokemon
-        if (pokemon.height !== maxHeight) {
-            li.appendChild(document.createTextNode(`${pokemon.name} (height: ${pokemon.height})`));
-        } else {
-            li.appendChild(document.createTextNode(`${pokemon.name} (height: ${pokemon.height}): This is the tallest  pokeman in the list`));
-        }
-
-        ul.appendChild(li);
-    }
-}
-
-function1();
+// representation of pokemon list in the web browser
+(function function1() {
+    pokemonRepository.getAll().forEach(function (pokemon) {pokemonRepository.addListItem(pokemon)});
+})();
